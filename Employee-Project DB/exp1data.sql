f3eb1ddb4238589eee22835e43a2329d8751e468
Shoaib Ahmed <shoaib30@gmail.com>
2015-04-01 23:37:32 +0530

    exp1

---
+++ Employee-Project DB/exp1data.sql
@@ -0,0 +1,164 @@
+insert into EMPLOYEE values
+(001,'Olivia Cruz',1);
+insert into EMPLOYEE values
+(002,'Marjorie Wells',2);
+insert into EMPLOYEE values
+(003,'Alicia Mitchell',3);
+insert into EMPLOYEE values
+(004,'Terri Ramos',4);
+insert into EMPLOYEE values
+(005,'Jeffrey Goodwin',5);
+insert into EMPLOYEE values
+(006,'Megan Park',2);
+insert into EMPLOYEE values
+(007,'Marian Hodges',4);
+insert into EMPLOYEE values
+(008,'Cindy Flores',1);
+insert into EMPLOYEE values
+(009,'Eunice Love',3);
+insert into EMPLOYEE values
+(010,'Danielle	Aguilar',5);
+insert into EMPLOYEE values
+(011,'Sharon	Nguyen',4);
+insert into EMPLOYEE values
+(012,'Kristen Cobb',1);
+insert into EMPLOYEE values
+(013,'Rodney	Frank',3);
+insert into EMPLOYEE values
+(014,'Brendan Guzman',2);
+insert into EMPLOYEE values
+(015,'Pete	Tran',5);
+insert into EMPLOYEE values
+(016,'Brooke Brock',3);
+insert into EMPLOYEE values
+(017,'Lila	Castillo',4);
+insert into EMPLOYEE values
+(018,'Randall Martinez',2);
+insert into EMPLOYEE values
+(019,'Shirley Andrews',5);
+insert into EMPLOYEE values
+(020,'George Phillips',1);
+
+insert into PROJECT values
+(020,'Database');
+insert into PROJECT values
+(021,'Algorithm Design');
+insert into PROJECT values
+(022,'System Networking');
+insert into PROJECT values
+(023,'Software Design');
+insert into PROJECT values
+(024,'Database');
+insert into PROJECT values
+(025,'Algorithm Design');
+insert into PROJECT values
+(026,'Debugging');
+insert into PROJECT values
+(027,'Virtual Reality');
+insert into PROJECT values
+(028,'Data Mining');
+insert into PROJECT values
+(029,'Cloud Computing');
+
+insert into ASSIGNTO values
+(001,020);
+insert into ASSIGNTO values
+(002,021);
+insert into ASSIGNTO values
+(003,022);
+insert into ASSIGNTO values
+(004,023);
+insert into ASSIGNTO values
+(005,024);
+insert into ASSIGNTO values
+(006,025);
+insert into ASSIGNTO values
+(007,026);
+insert into ASSIGNTO values
+(008,027);
+insert into ASSIGNTO values
+(009,028);
+insert into ASSIGNTO values
+(010,029);
+insert into ASSIGNTO values
+(011,020);
+insert into ASSIGNTO values
+(012,021);
+insert into ASSIGNTO values
+(013,022);
+insert into ASSIGNTO values
+(014,023);
+insert into ASSIGNTO values
+(015,024);
+insert into ASSIGNTO values
+(016,025);
+insert into ASSIGNTO values
+(017,026);
+insert into ASSIGNTO values
+(018,027);
+insert into ASSIGNTO values
+(019,028);
+insert into ASSIGNTO values
+(001,029);
+insert into ASSIGNTO values
+(002,028);
+insert into ASSIGNTO values
+(003,027);
+insert into ASSIGNTO values
+(004,026);
+insert into ASSIGNTO values
+(005,025);
+insert into ASSIGNTO values
+(006,024);
+insert into ASSIGNTO values
+(007,023);
+insert into ASSIGNTO values
+(008,022);
+insert into ASSIGNTO values
+(009,021);
+insert into ASSIGNTO values
+(010,020);
+insert into ASSIGNTO values
+(011,029);
+insert into ASSIGNTO values
+(012,028);
+insert into ASSIGNTO values
+(013,027);
+insert into ASSIGNTO values
+(014,026);
+insert into ASSIGNTO values
+(015,025);
+insert into ASSIGNTO values
+(016,024);
+insert into ASSIGNTO values
+(017,023);
+insert into ASSIGNTO values
+(018,022);
+insert into ASSIGNTO values
+(019,021);
+insert into ASSIGNTO values
+(001,023);
+insert into ASSIGNTO values
+(002,026);
+insert into ASSIGNTO values
+(003,029);
+insert into ASSIGNTO values
+(004,022);
+insert into ASSIGNTO values
+(005,027);
+insert into ASSIGNTO values
+(006,028);
+insert into ASSIGNTO values
+(007,021);
+insert into ASSIGNTO values
+(008,024);
+insert into ASSIGNTO values
+(009,027);
+insert into ASSIGNTO values
+(001,026);
+insert into ASSIGNTO values
+(002,024);
+insert into ASSIGNTO values
+(001,028);
+insert into ASSIGNTO values
+(001,025);
