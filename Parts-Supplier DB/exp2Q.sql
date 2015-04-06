c47542c58deb8d3fc2a9b5ee5374b06d8bfef599
Shoaib Ahmed <shoaib30@gmail.com>
2015-04-06 22:15:14 +0530

    updated 5

--- Parts-Supplier DB/exp2Q.sql
+++ Parts-Supplier DB/exp2Q.sql
@@ -12,4 +12,17 @@ PART.PNAME='Bolt';
 
 REMARK c.	Delete the parts which are green in colour;
 delete from SUPPLY (select SUPPLY.PNO from SUPPLY, PART where PART.PNO=SUPPLY.PNO and PART.COLOR='green');
-delete from PART where color='green';
+delete from PART where colour='green';
+
+REMARK d.	Find the parts that are supplied from Bengaluru;
+select SUPPLY.PNO, PART.PNAME 
+from SUPPLIER, SUPPLY, PART
+where SUPPLY.PNO=PART.PNO and
+SUPPLY.SNO=SUPPLIER.SNO and
+SUPPLIER.ADDRESS='Bengaluru';
+
+REMARK e.	Retrieve the SNO of the Supplier who supplies only Blue parts;
+select SUPPLY.SNO 
+from SUPPLY,PART
+where SUPPLY.PNO=PART.PNO and PART.COLOUR='blue' and 
+SUPPLY.SNO NOT IN(select SUPPLY.SNO from SUPPLY,PART where SUPPLY.PNO=PART.PNO and PART.COLOUR!='blue' );
