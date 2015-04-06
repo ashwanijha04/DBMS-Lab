f962ce50103eedb8665b93b9be92e70d190d76ba
Shoaib Ahmed <shoaib30@gmail.com>
2015-04-06 21:42:11 +0530

    updated all 5

--- Employee-Project DB/exp1Q.sql
+++ Employee-Project DB/exp1Q.sql
@@ -12,3 +12,15 @@ group by DEPTNO;
 
 REMARK c.	Update the ProjectNo of Employee bearing SSN=1 to ProjectNo=20;
 update ASSIGNTO set PROJNO=20 where SSN=1;
+
+REMARK d.	Obtain the SSN of the employee assigned to project 2 and 4;
+select SSN from ASSIGNTO where PROJNO=22
+UNION
+select SSN from ASSIGNTO where PROJNO=24;
+
+REMARK e. Find the name of the employee working on database;
+select EMPLOYEE.NAME 
+from EMPLOYEE, ASSIGNTO, PROJECT
+where EMPLOYEE.SSN=ASSIGNTO.SSN and
+PROJECT.PROJNO=ASSIGNTO.PROJNO and 
+PROJECT.PROJAREA='Database';
