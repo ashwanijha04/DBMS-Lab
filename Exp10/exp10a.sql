079cec75a6ee3f67d18246b33e0c0d6212552d65
Shoaib Ahmed <shoaib30@gmail.com>
2015-05-01 10:58:10 +0530

    add

---
+++ Exp10/exp10a.sql
@@ -0,0 +1,26 @@
+SET SERVEROUTPUT ON;
+
+CREATE or REPLACE procedure FACT(N in number, F out number)
+AS
+I number;
+
+BEGIN
+	F:=1;
+	for I in 1..N
+	loop
+		F:=F*I;
+	end loop;
+END;
+/
+
+DECLARE
+N number:=&n;
+F number;
+
+BEGIN
+FACT(N,F);
+DBMS_OUTPUT.PUT_LINE('Factorial of '||N||' is: '||F);
+
+END;
+/
+
---
+++ Exp10/exp10b.sql
@@ -0,0 +1,33 @@
+create table  employee1(ssn varchar(20),name char(20),deptno number(10),salary int,primary key(ssn));
+
+insert into employee1 values(1,'pra',10,1000);
+insert into employee1values(2,'aaa',20,2000);
+insert into employee1 values(3,'bbb',10,4000);
+insert into employee1 values(4,'cc',30,10000);
+insert into employee1 values(5,'bgg',10,2000);
+
+select *from employee1;
+
+
+SET SERVEROUTPUT ON;
+
+DECLARE
+cursor cur is select *from employee1;
+rows employee1%rowtype;
+t int;
+
+BEGIN
+open cur;
+t:= &t1;
+loop 
+fetch cur into rows;
+exit when cur%notfound;
+if rows.deptno=t
+then
+dbms_output.put_line('the ssn and names are '|| rows.ssn ||','||rows.name);
+end if;
+end loop;
+close cur;
+
+END;
+/
